1f6e32dda4310a993ce723ea92a9b5c5a044e969
Arthi Halder <arthihalder@gmail.com>
2026-07-28 13:45:21 +0600

    backup16

--- code/level/area_3_sub_1_to_area_3.gd
+++ code/level/area_3_sub_1_to_area_3.gd
@@ -1,5 +1,15 @@
 extends Area2D
 
+var player_inside := false
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
+		player_inside = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		player_inside = false
+
+func _process(delta):
+	if player_inside and Input.is_action_just_pressed("next"):
 		get_tree().change_scene_to_file("res://area/area_3.tscn")
