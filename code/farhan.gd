b86d54dc088138efb83cba2aea1bc7c7a79b2e23
Arthi Halder <arthihalder@gmail.com>
2026-07-28 10:12:50 +0600

    backup 15

---
+++ code/farhan.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+@onready var path_in: PathFollow2D = $"../DiponIn/PathFollow2D"
+@onready var path_out: PathFollow2D = $"../DiponOut/PathFollow2D"
+var current_path: PathFollow2D
+@export var move_speed: float = 50.0
+
+func _ready() -> void:
+	GameState.stage_changed.connect(_on_stage_changed)
+
+func _on_stage_changed():
+
+	if GameState.stage == 12:
+		current_path = path_in
+		current_path.progress = 0
+
+	elif GameState.stage == 17:
+		current_path = path_out
+		current_path.progress = 0
+		
+func _physics_process(delta: float) -> void:
+	if current_path:
+		current_path.progress += move_speed * delta
+		global_position = current_path.global_position
+		
